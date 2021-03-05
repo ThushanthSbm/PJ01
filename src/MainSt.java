81900a0aaab6f84c210338438e108d0b2293d21b
ThushanthSbm <56014745+ThushanthSbm@users.noreply.github.com>
2021-03-05 23:03:38 +0530

    First One.........

---
+++ src/MainSt.java
@@ -0,0 +1,20 @@
+
+public class MainSt {
+
+static int x = 10;
+	
+	public static void main(String args[]){
+		
+		
+	//	St s = new St();
+	 /*   St.id=20;
+		St.display();
+		St s = new St();
+		//St.display();
+		s.display();
+		St.id = 30;
+		s.display();*/
+		
+		System.out.println(x);
+	}
+}
---
+++ src/St.java
@@ -0,0 +1,13 @@
+
+public class St {
+
+static int id = 10;
+	
+	St(){
+		this.id = 100;
+		
+	}
+	static void display(){
+		System.out.println("ID iS :"+id);
+	}
+}
